80319e795f7485292536d633a45e7e96679d71b9
tatsui <tatsuya.ito.at@gmail.com>
2023-01-02 15:08:15 +0000

    first commit

---
+++ docker/app/main.py
@@ -0,0 +1,102 @@
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+import requests
+import json
+import base64
+
+
+def parse_jwt(jwt):
+    payload = {"sub": "test_user", "name": "hoge"}
+    try:
+        tmp = jwt.split(".")
+        payload = json.loads(base64.b64decode(tmp[1]).decode())
+    except Exception as e:
+        print(e)
+    return payload
+
+
+class BaseHttpServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        payload = parse_jwt(self.headers["Authorization"])
+        post_json = {
+            "requestContext": {
+                "authorizer": {
+                    "principalId": payload["sub"],
+                }
+            },
+            "headers": {
+                "Authorization": "test_token"
+            },
+            "resource": self.path,
+            "httpMethod": "GET",
+            "body": "",
+        }
+        r = requests.post(
+            "http://lambda:8080/2015-03-31/functions/function/invocations",
+            data=json.dumps(post_json),
+        )
+        resp = r.json()
+        self.send_response(resp["statusCode"])
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header(
+            "Access-Control-Allow-Headers",
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Amzn-Trace-Id",
+        )
+        self.end_headers()
+        body = resp["body"]
+        self.wfile.write(body.encode())
+
+    def do_POST(self):
+        payload = parse_jwt(self.headers["Authorization"])
+        content_len = int(self.headers.get("content-length").encode())
+        requestBody = self.rfile.read(content_len).decode("utf-8")
+        post_json = {
+            "requestContext": {
+                "authorizer": {
+                    "principalId": payload["sub"],
+                }
+            },
+            "headers": {
+                "Authorization": "test_token"
+            },
+            "resource": self.path,
+            "httpMethod": "POST",
+            "body": requestBody,
+        }
+        r = requests.post(
+            "http://lambda:8080/2015-03-31/functions/function/invocations",
+            data=json.dumps(post_json),
+        )
+        resp = r.json()
+        body = resp["body"]
+        self.send_response(resp["statusCode"])
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header(
+            "Access-Control-Allow-Headers",
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Amzn-Trace-Id",
+        )
+        self.end_headers()
+        self.wfile.write(body.encode())
+
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header("content-length", 0)
+        self.send_header("Content-type", "application/json")
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header(
+            "Access-Control-Allow-Headers",
+            "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent,X-Amzn-Trace-Id",
+        )
+        self.end_headers()
+
+
+ip = "0.0.0.0"
+port = 8080
+
+server = HTTPServer((ip, port), BaseHttpServer)
+server.serve_forever()
